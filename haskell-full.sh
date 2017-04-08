f2c91057ac9ee2b2ef3563013386a45d84da87a6
Ryan Gonzalez <rymg19@gmail.com>
2017-04-08 12:46:03 -0500

    Oops...

--- haskell-full.sh
+++ haskell-full.sh
@@ -1,5 +1,2 @@
-# :DEPENDS git:project-rootbox/rootbox-factories///priv/haskell-full-download.sh
-# :DEPENDS git:project-rootbox/rootbox-factories///priv/haskell-install.sh
-
-#:DEPENDS priv/haskell-full-download.sh
-#:DEPENDS priv/haskell-install.sh
+#:DEPENDS git:project-rootbox/rootbox-factories///priv/haskell-full-download.sh
+#:DEPENDS git:project-rootbox/rootbox-factories///priv/haskell-install.sh

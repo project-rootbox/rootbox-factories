ca2a1812910b364e36b64aca301fa9e8a77552c1
Ryan Gonzalez <rymg19@gmail.com>
2017-04-10 12:47:42 -0500

    Formatting tweaks

--- general/glibc.sh
+++ general/glibc.sh
@@ -2,9 +2,9 @@
 
 REPO=sgerrand/alpine-pkg-glibc
 VER=2.25-r0
+KEY=https://raw.githubusercontent.com/$REPO/master/sgerrand.rsa.pub
+APK=https://github.com/$REPO/releases/download/$VER/glibc-$VER.apk
 
-sudo curl -fo /etc/apk/keys/sgerrand.rsa.pub \
-    https://raw.githubusercontent.com/$REPO/master/sgerrand.rsa.pub
-curl -Lfo glibc.apk \
-    https://github.com/$REPO/releases/download/$VER/glibc-$VER.apk
+sudo curl -fo /etc/apk/keys/sgerrand.rsa.pub $KEY
+curl -Lfo glibc.apk $APK
 sudo apk add glibc.apk

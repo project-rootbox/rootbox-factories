b64946302e4fd1925ed51d0f3b503961ab550311
Ryan Gonzalez <rymg19@gmail.com>
2017-06-25 17:45:25 -0500

    GTK3 stuff

--- dev/gtk3-static.sh
+++ dev/gtk3-static.sh
@@ -30,7 +30,7 @@ GTK_MIN_VER=3.22.12
 GTK_SOURCES=$SOURCES/$GTK/$GTK_MAJ_VER/$GTK-$GTK_MIN_VER.tar.xz
 
 sudo apk add xz glib-static@edge glib-dev@edge flex bison python2-dev \
-                tiff-dev cairo-dev
+                tiff-dev cairo-dev libxi-dev
 
 curl -Lfo gobj.txz $GOBJ_SOURCES
 tar xvf gobj.txz
